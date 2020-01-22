a65657ba1af718b9ab67de2dba7fd709ecaa08f6
vcutrona <vcutrona1@gmail.com>
2020-01-22 17:13:03 +0100

    Add wan-only configurable option to ElasticConfig

--- elasticpedia/config/elastic_conf.py
+++ elasticpedia/config/elastic_conf.py
@@ -9,10 +9,12 @@ class ElasticConfig:
                  nodes: str = "localhost",
                  port: int = 9200,
                  input_json: str = "yes",
+                 wan_only: str = "false",
                  mapping_id: str = None):
 
         self._nodes = os.getenv("ELASTIC_NODES") if os.getenv("ELASTIC_NODES") else nodes
         self._port = os.getenv("ELASTIC_PORT") if os.getenv("ELASTIC_PORT") else port
+        self._wan_only = os.getenv("ELASTIC_WAN_ONLY") if os.getenv("ELASTIC_WAN_ONLY") else wan_only
         self._resource = resource
         self._input_json = input_json
         self._mapping_id = mapping_id
@@ -34,6 +36,7 @@ class ElasticConfig:
             "es.port": f'{self._port}',
             "es.resource": self._resource,
             "es.input.json": self._input_json,
+            "es.nodes.wan.only": self._wan_only
         }
 
         if self._mapping_id:
