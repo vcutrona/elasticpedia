e01c91aa86a0fbdaf25b8df0cd30b2b6441251de
vcutrona <vcutrona1@gmail.com>
2020-03-11 17:48:09 +0100

    Remove non-compatible versions of Python 3

--- setup.py
+++ setup.py
@@ -19,17 +19,15 @@ test_requirements = []
 setup(
     author="Vincenzo Cutrona",
     author_email='v.cutrona1@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6, <3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     description="Indexing DBpedia with Elasticsearch.",
     entry_points={
